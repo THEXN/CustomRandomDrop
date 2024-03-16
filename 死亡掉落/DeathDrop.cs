dbb9db1ea4e59458ecaad94c2cca732f141c4153
肝帝熙恩 <111548550+THEXN@users.noreply.github.com>
2024-03-16 14:17:48 +0800

    Update README.md

--- 死亡掉落/DeathDrop.cs
+++ 死亡掉落/DeathDrop.cs
@@ -4,22 +4,22 @@ using Terraria;
 using TerrariaApi.Server;
 using TShockAPI;
 
-namespace 死亡掉落
+namespace DeathDrop
 {
     [ApiVersion(2, 1)]
-    public class 死亡掉落 : TerrariaPlugin
+    public class DeathDrop : TerrariaPlugin
     {
         public static Random RandomGenerator = new Random();
 
-        public override string Author => "大豆子";
+        public override string Author => "大豆子，肝帝熙恩更新优化";
 
         public override string Description => "自定义怪物死亡随机掉落物";
 
         public override string Name => "死亡随机掉落";
 
-        public override Version Version => new Version(1, 0, 0, 0);
+        public override Version Version => new Version(1, 0, 1);
 
-        public 死亡掉落(Main game) : base(game)
+        public DeathDrop(Main game) : base(game)
         {
         }
 
@@ -38,17 +38,17 @@ namespace 死亡掉落
 
             try
             {
-                死亡掉落配置表 config = 死亡掉落配置表.GetConfig();
+                DeathDropConfig config = DeathDropConfig.GetConfig();
 
-                if (config.是否开启随机掉落)
+                if (config.EnableRandomDrops)
                 {
                     int itemId;
-                    if (config.完全随机掉落)
+                    if (config.FullRandomDrops)
                     {
                         // 从1到5452中随机选择一个物品ID
                         itemId = RandomGenerator.Next(1, 5453);
                         // 检查这个ID是否在排除列表中
-                        while (config.完全随机掉落排除物品ID.Contains(itemId))
+                        while (config.FullRandomExcludedItems.Contains(itemId))
                         {
                             // 如果是在排除列表中，则重新选择
                             itemId = RandomGenerator.Next(1, 5453);
@@ -57,14 +57,14 @@ namespace 死亡掉落
                     else
                     {
                         // 否则从配置的随机掉落物列表中随机选择一个物品 ID
-                        int randomIndex = RandomGenerator.Next(config.普通随机掉落物.Count);
-                        itemId = config.普通随机掉落物[randomIndex];
+                        int randomIndex = RandomGenerator.Next(config.CommonRandomDrops.Count);
+                        itemId = config.CommonRandomDrops[randomIndex];
                     }
-                    if (Candorp(config.随机掉落概率))
+                    if (Candorp(config.RandomDropChance))
                     {
                         Item item = TShock.Utils.GetItemById(itemId);
 
-                            int dropAmount = RandomGenerator.Next(config.随机掉落数量最小值, config.随机掉落数量最大值); // 随机生成1到物品最大堆叠数量的物品数量
+                            int dropAmount = RandomGenerator.Next(config.MinRandomDropAmount, config.MaxRandomDropAmount); // 随机生成1到物品最大堆叠数量的物品数量
                         int itemNumber = Item.NewItem(
                             null,
                             (int)args.npc.position.X,
@@ -83,20 +83,36 @@ namespace 死亡掉落
                     }
                 }
 
-                if (!config.是否开启自定义掉落)
+                if (!config.EnableCustomDrops)
                 {
                     return;
                 }
 
-                foreach (死亡掉落配置表.怪物 monster in config.自定义掉落设置)
+                foreach (DeathDropConfig.Monster monster in config.DeathDropSet)
                 {
-                    if (monster.怪物ID == npcNetID && Candorp(monster.掉落概率))
+                    if (monster.NPCID == npcNetID && Candorp(monster.DropChance))
                     {
+                        int dropItemId;
+                        if (monster.FullRandomDrops)
+                        {
+                            // 从1到5452中随机选择一个物品ID
+                            dropItemId = RandomGenerator.Next(1, 5453);
+                            // 检查这个ID是否在排除列表中
+                            while (monster.FullRandomExcludedItems.Contains(dropItemId))
+                            {
+                                // 如果是在排除列表中，则重新选择
+                                dropItemId = RandomGenerator.Next(1, 5453);
+                            }
+                        }
+                        else
+                        {
+                            // 否则从配置的随机掉落物列表中随机选择一个物品 ID
+                            int randomIndex = RandomGenerator.Next(monster.CommonRandomDrops.Count);
+                            dropItemId = monster.CommonRandomDrops[randomIndex];
+                        }
                         // 从怪物.掉落物列表中随机选择一个物品 ID
-                        int randomIndex = RandomGenerator.Next(monster.掉落物列表.Count);
-                        int dropItemId = monster.掉落物列表[randomIndex];
                         Item dropItem = TShock.Utils.GetItemById(dropItemId);
-                        int dropAmount = RandomGenerator.Next(monster.随机掉落数量最小值, monster.随机掉落数量最大值 + 1);
+                        int dropAmount = RandomGenerator.Next(monster.RandomDropMinAmount, monster.RandomDropMaxAmount + 1);
                         int dropItemNumber = Item.NewItem(
                             null,
                             (int)npcPosition.X,
